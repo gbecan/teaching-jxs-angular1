eb35cc2edd38609cd622fb77161296fc116d6dc1
cheisda <cheisda@gmail.com>
2016-04-14 13:28:57 +0200

    partly finished. up to question 12-14

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,3 +5,31 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 }]);
 
 var pokeApiUrl = "http://pokeapi.co/"
+
+/**Factory**/
+pokeApp.factory("PokemonCheisoudou", function($resource){
+    return $resource("http://pokeapi.co/api/v2/pokemon-species/:id/",{pokemonID:'@id'});
+    });
+
+/**Controller**/
+pokeApp.controller('pokeController', function($scope, $log,PokemonCheisoudou, $http, $resource){
+    $scope.log=function(){
+    return console.log()
+    };
+
+    $http({
+        method: 'GET',
+        url:pokeApiUrl+'api/v2/pokedex/1/'
+        }).then(function successfulCallback(response){
+            $scope.pokemonList = response.data.pokemon_entries;
+            console.log(response.data.pokemon_entries);
+            }, function errorCallback(response){
+                $scope.log="Error";
+                });
+
+            $scope.envoyer=function(pokemonID){
+            console.log("Hallo");
+            console.log(pokemonID);
+            $scope.pokemonDisplay=PokemonCheisoudou.get();
+            }
+})//fin du pokeController
